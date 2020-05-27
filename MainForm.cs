d7d70dcdc548a0b353c3ff251e7334db2b4a2b42
AlexF1404 <36068691+AlexF1404@users.noreply.github.com>
2020-05-27 22:30:19 +0300

    Рабочая версия Excel и Word

--- MainForm.cs
+++ MainForm.cs
@@ -36,7 +36,6 @@ namespace VKRProjectUipath
         
         private void BtnExcel_Click(object sender, EventArgs e)
         {
-            InitializeComponent();
             pathsExcel.Clear();                  
             InitializeOpenFileDialog("Файлы Excel (*.xl*;*.xlsx;*.xlsm;*.xlsb;*.xlam;*.xltx;*.xltm;*.xls;*.xla;*.xlt;*.xlm;*.xlw;)|*.xl*;*.xlsx;*.xlsm;*.xlsb;*.xlam;*.xltx;*.xltm;*.xls;*.xla;*.xlt;*.xlm;*.xlw;", "Выберите Excel файлы учебных планов");          
             pathsExcel = selectListOfPathsInOpenFileDialog();
@@ -64,62 +63,57 @@ namespace VKRProjectUipath
         {
             InitializeOpenFileDialog("Все документы Word и Pdf (*.doc;*.docm;*.dotx;*.dotm;*.doc;*.dot;*.htm;*.html;*.rtf;*.mht;*.mhtml;*.xml;*.odt;*.pdf;)|*.doc;*.docm;*.dotx;*.dotm;*.doc;*.dot;*.htm;*.html;*.rtf;*.mht;*.mhtml;*.xml;*.odt;*.pdf;", "Выберите Word/Pdf файлы курсовых работ");
             pathsWordPdf = selectListOfPathsInOpenFileDialog();
-            Dictionary<string, string> keysforGr = GoDBForDictionary();
-            foreach (KeyValuePair<string, string> keys in keysforGr)
-            {
-                Console.WriteLine(keys.Key);
-                Console.WriteLine(keys.Value);
-            }
+            Dictionary<string, string> keysforGr = GoDBForDictionary();           
             List<string> nameOfPredmets = GetNameOfPredmets();
             VRKWordPdfSort(keysforGr,nameOfPredmets);
         }
         private List<string> GetNameOfPredmets() 
         {
-            string dbFileNames = "dbForNameGroup";
-            string tableName = "namedirect";
-            SQLiteConnection m_dbConn = OpenOrCreateDataBase(dbFileNames,tableName);
-            DataTable dTable = new DataTable();
-            List<string> nameofDirectionToBe = new List<string>();
-            string sqlQuery = "SELECT * FROM namedirect";         
-            SQLiteDataAdapter adapter = new SQLiteDataAdapter(sqlQuery, m_dbConn);
-            adapter.Fill(dTable);
-            m_dbConn.Close();
-            if (dTable.Rows.Count >= 0)
-            {
-                for (int i = 0; i < dTable.Rows.Count; i++)
+                string dbFileNames = "dbForNameGroup";
+                string tableName = "namedirect";
+                SQLiteConnection m_dbConn = OpenOrCreateDataBase(dbFileNames,tableName);
+                DataTable dTable = new DataTable();
+                List<string> nameofDirectionToBe = new List<string>();
+                string sqlQuery = "SELECT * FROM namedirect";         
+                SQLiteDataAdapter adapter = new SQLiteDataAdapter(sqlQuery, m_dbConn);
+                adapter.Fill(dTable);
+                m_dbConn.Close();
+                if (dTable.Rows.Count >= 0)
                 {
-                    nameofDirectionToBe.Add(dTable.Rows[i].Field<string>(1));
-                    Console.WriteLine("LOL "+ dTable.Rows[i].Field<string>(1));
+                    for (int i = 0; i < dTable.Rows.Count; i++)
+                    {
+                        nameofDirectionToBe.Add(dTable.Rows[i].Field<string>(1));
+                        Console.WriteLine("LOL "+ dTable.Rows[i].Field<string>(1));
+                    }
+                    return nameofDirectionToBe;
                 }
                 return nameofDirectionToBe;
-            }
-            return nameofDirectionToBe;
 
         }
         private SQLiteConnection OpenOrCreateDataBase(string DbName,string TableName)
         {
-            SQLiteConnection m_dbConn = new SQLiteConnection();
-            SQLiteCommand m_sqlCmd = new SQLiteCommand();
-            string dbFileNames = DbName;
-            if (!File.Exists(dbFileNames))
-            {
-                SQLiteConnection.CreateFile(dbFileNames);
-                Console.WriteLine("БЫЛ СОЗДАН");
-            }
-            try
-            {
-                m_dbConn = new SQLiteConnection("Data Source=" + dbFileNames + ";Version=3;");
-                m_dbConn.Open();
-                m_sqlCmd.Connection = m_dbConn;
-                m_sqlCmd.CommandText = "CREATE TABLE IF NOT EXISTS "+TableName+" (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)";
-                m_sqlCmd.ExecuteNonQuery();
-                return m_dbConn;
-            }
-            catch (SQLiteException ex)
-            {
-                MessageBox.Show("");
-                return m_dbConn;
-            }
+                SQLiteConnection m_dbConn = new SQLiteConnection();
+                SQLiteCommand m_sqlCmd = new SQLiteCommand();
+                string dbFileNames = DbName;
+                if (!File.Exists(dbFileNames))
+                {
+                    SQLiteConnection.CreateFile(dbFileNames);
+                    Console.WriteLine("БЫЛ СОЗДАН");
+                }
+                try
+                {
+                    m_dbConn = new SQLiteConnection("Data Source=" + dbFileNames + ";Version=3;");
+                    m_dbConn.Open();
+                    m_sqlCmd.Connection = m_dbConn;
+                    m_sqlCmd.CommandText = "CREATE TABLE IF NOT EXISTS "+TableName+" (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)";
+                    m_sqlCmd.ExecuteNonQuery();
+                    return m_dbConn;
+                }
+                catch (SQLiteException ex)
+                {
+                    MessageBox.Show("");
+                    return m_dbConn;
+                }
            
         }
         private void AddDBNameOfDirection(List<string> addNameOfPredmets)
@@ -144,40 +138,32 @@ namespace VKRProjectUipath
                     string tableName = "namedirect";
                     SQLiteConnection m_dbConn = OpenOrCreateDataBase(dbFileNames, tableName);
                     SQLiteCommand m_sqlCmd = new SQLiteCommand();
-
                     m_sqlCmd.Connection = m_dbConn;
                     string sqlrefresh = "DELETE FROM namedirect";
                     m_sqlCmd.CommandText = sqlrefresh;
                     m_sqlCmd.ExecuteNonQuery();
-
-
                     string sqlGo = "INSERT INTO namedirect (name) VALUES ";
                     foreach (string item in namePredmetsToBe)
                     {
-                        sqlGo += "( '" + item + "' ),";
-                        Console.WriteLine("KYK" + item);
+                        sqlGo += "( '" + item + "' ),";                      
                     }
 
                     sqlGo = sqlGo.Remove(sqlGo.Length - 1);
                     try
                     {
-
                         m_sqlCmd.CommandText = sqlGo;
                         m_sqlCmd.ExecuteNonQuery();
                         m_dbConn.Close();
-
                     }
                     catch (SQLiteException)
                     {
-                        MessageBox.Show("Ваша база пуста. Добавьте записи.");
-
+                        MessageBox.Show("Ошибка при заполнении бд");
                     }
                 }
 
                 catch (SQLiteException ex)
                 {
                     MessageBox.Show("Error: " + ex.Message);
-
                 }
             }
         }                      
@@ -205,7 +191,7 @@ namespace VKRProjectUipath
         }          
         private string VRKExcelAndClearList() {
          
-          string cmd = @"UiRobot.exe connect --url https://cloud.uipath.com/alexcompany/AlexCompanyDefault --key 51595987-ee0a-426e-9cae-bd63f34311be";
+            string cmd = @"UiRobot.exe connect --url https://cloud.uipath.com/alexcompany/AlexCompanyDefault --key 51595987-ee0a-426e-9cae-bd63f34311be";
             string pathStringExcel = JsonConvert.SerializeObject(pathsExcel, Formatting.None);
             string pathFolder = JsonConvert.SerializeObject(Properties.Settings.Default.PathStringFolder, Formatting.None);
             string cmd1 = @"UiRobot.exe execute --process VKRExcel --input " + "\"" + "{" + "\'" + "pathStringExcel" + "\'" + ": " + pathStringExcel.Replace("\"", "'").Replace("\\x22", "\\\"") + ", " + "\'" + "pathFolder" + "\'" + ": " + pathFolder.Replace("\"", "'").Replace("\\x22", "\\\"") + "}\"";
@@ -229,15 +215,14 @@ namespace VKRProjectUipath
                 startInfo.CreateNoWindow = true;              
                 startInfo.StandardOutputEncoding = Encoding.GetEncoding(866);
             Process procCommand = new Process();
-               procCommand.OutputDataReceived += new DataReceivedEventHandler(ProcessOutputHandler);
-               procCommand.ErrorDataReceived += new DataReceivedEventHandler(ProcessOutputHandler);
-               procCommand.StartInfo = startInfo;
-               procCommand.Start();
-               procCommand.WaitForExit();           
+                procCommand.OutputDataReceived += new DataReceivedEventHandler(ProcessOutputHandler);
+                procCommand.ErrorDataReceived += new DataReceivedEventHandler(ProcessOutputHandler);
+                procCommand.StartInfo = startInfo;
+                procCommand.Start();
+                procCommand.WaitForExit();           
             StreamReader srIncoming = procCommand.StandardOutput;
-            Console.WriteLine("wwww+   "+srIncoming.ReadToEnd().ToString());                                                       
-            
-            return srIncoming.ReadToEnd();
+            string json = srIncoming.ReadToEnd().ToString();           
+            return json;
         }
         private static void ProcessOutputHandler(object sendingProcess, DataReceivedEventArgs outLine)
             {
@@ -253,12 +238,7 @@ namespace VKRProjectUipath
        
         private void VRKWordPdfSort(Dictionary<string,string> keysForGroup,List<string> namePredmetsList)
         {            
-            string cmd = @"UiRobot.exe connect --url https://cloud.uipath.com/alexcompany/AlexCompanyDefault --key 51595987-ee0a-426e-9cae-bd63f34311be";
-            /* Dictionary<string, string> keys = new Dictionary<string, string>();
-             //keys.Add("ИТ", "Информационные системы и технологии");
-             keys.Add("ит", "Информационные системы и технологии");*/
-            /*List<string> newstring = new List<string>();
-            newstring.Add("Управление IT-проектами");*/
+            string cmd = @"UiRobot.exe connect --url https://cloud.uipath.com/alexcompany/AlexCompanyDefault --key 51595987-ee0a-426e-9cae-bd63f34311be";         
             string dictionaryabrv = JsonConvert.SerializeObject(keysForGroup, Formatting.None);
             string pathFolder = JsonConvert.SerializeObject(Properties.Settings.Default.PathStringFolder, Formatting.None);
             string PathStringFileWordPdf = JsonConvert.SerializeObject(pathsWordPdf, Formatting.None);
@@ -298,7 +278,7 @@ namespace VKRProjectUipath
         }
         private void settingsItem_Click(object sender, EventArgs e)
         {
-            Settings settings = new Settings();
+            Settings settings = new Settings();           
             settings.Show();
         }
         private Dictionary<string,string> GoDBForDictionary()
